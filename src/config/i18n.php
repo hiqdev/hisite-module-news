a54623df9fdb7742b224c18680058192d9d6fb7a
tofid <andreyklochok@gmail.com>
2016-10-05 19:22:40 +0300

    Added i18n config

---
+++ src/config/i18n.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'sourcePath' => '@hisite/modules/news',
+    'languages' => ['ru'],
+    'removeUnused' => false,
+    'markUnused' => false,
+    'sort' => true,
+    'messagePath' => '@hisite/modules/news/messages',
+    'ignoreCategories' => [
+//        'app',
+//        'cart',
+//        'hisite',
+//        'hipanel/server/order',
+//        'hipanel/server/os',
+//        'hipanel/domain',
+//        'hipanel/domainchecker',
+//        'hipanel/site',
+//        'hipanel/pages',
+//        'hipanel/vds',
+    ]
+];
